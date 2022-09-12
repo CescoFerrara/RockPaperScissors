9884b6ce880f1abfdba17425a9a2dd39f86878d0
Francesco Ferrara <fferrar4@jhu.edu>
2022-09-12 17:50:43 -0400

    Added a leveling system

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const choices = [
     "scissors"
 ]
 
-let W = 0;
-let L = 0;
-let D = 0;
+
 
 const result = document.getElementById("result");
 const message = document.getElementById("message");
 
-const scoreboard = document.getElementById("scoreboard");
+let expMax = 1;
+let expCurrent = 0;
+
 
 function play(event) {
     const userChoice = event.target.id;
@@ -23,25 +23,41 @@ function play(event) {
 
   if (computerChoice === userChoice) {
     result.innerHTML = "Draw!";
-    D = D + 1;
   } else if (computerChoice === "rock" && userChoice === "paper") {
     result.innerHTML = "You win!";
-    W = W + 1;
+    updateExp();
   } else if (computerChoice === "rock" && userChoice === "scissors") {
     result.innerHTML = "You lost!";
-    L = L + 1;
   } else if (computerChoice === "paper" && userChoice === "scissors") {
     result.innerHTML = "You win!";
-    W = W + 1;
+    updateExp();
   } else if (computerChoice === "paper" && userChoice === "rock") {
     result.innerHTML = "You lose!";
-    L = L + 1;
   } else if (computerChoice === "scissors" && userChoice === "rock") {
     result.innerHTML = "You win!";
-    W = W + 1;
+    updateExp();
   } else if (computerChoice === "scissors" && userChoice === "paper") {
     result.innerHTML = "You lose!";
-    L = L + 1;
   }
-  scoreboard.innerHTML = "W: " + W + " | L: " + L + " | D: " + D;
+  
+
+
+}
+
+function updateExp() {
+  expCurrent = expCurrent + 1;
+  if (expCurrent === expMax) {
+    document.getElementById("ExpBar").style.width = 0 + "%";
+    document.getElementById("ExpBar").setAttribute("aria-valuenow", 0);
+    expMax = expMax + 1;
+    expCurrent = 0;
+    document.getElementById("ExpLeft").innerHTML = "0/" + expMax + " to Level " + expMax;
+    result.innerHTML = "You win! Level Up!";
+  } else {
+    let percentage = ((expCurrent / expMax) * 100);
+    document.getElementById("ExpBar").style.width = percentage + "%";
+    document.getElementById("ExpBar").setAttribute("aria-valuenow", percentage);
+    width = percentage + "%";
+    document.getElementById("ExpLeft").innerHTML = expCurrent + "/" + expMax + " to Level " + expMax;
+  }
 }
